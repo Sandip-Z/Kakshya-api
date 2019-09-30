3ec49c8d33313aace2a613366b488f0ec7c17e2f
Sandip Satyal <offmtrznephop@gmail.com>
2019-09-30 21:47:11 +0545

    Connect to local database

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser =  require('body-parser');
 const signinRoutes = require('./Routes/signinRoutes');
 const signupRoutes = require('./Routes/signupRoutes');
 
 var jsonParser = bodyParser.json();
+
+//database connection
+mongoose.connect('mongodb://localhost/kakshya', { useNewUrlParser: true, useUnifiedTopology:true });
+
+mongoose.connection.once('open', ()=>{
+    console.log('connected to database')
+})
+.on('error', (err)=>{
+    console.log('error occured while connecting to database', err)
+})
+
 
 app.use('/api/signin',jsonParser,signinRoutes);
 app.use('/api/signup',jsonParser,signupRoutes);
