7afbebc3af2954e8c2c7485f47381dab04a8c1c7
Sandip Satyal <offmtrznephop@gmail.com>
2019-09-30 22:40:17 +0545

    Deprecate manual api_key_check

--- Controller/signupController.js
+++ Controller/signupController.js
@@ -1,5 +1,4 @@
 const clientAuth = require('../utils/Auth/clientAuth');
-const apiKeyCheck = require('../utils/Auth/apiKeyCheck');
 var accident = require('../utils/Error/accident');
 
 class signupController{
@@ -13,36 +12,26 @@ class signupController{
 
     POST_signup_ROOT(req, res){
         var parcel = req.body;
-        // console.log(parcel);
-        let key = parcel.api_key;
-        // console.log(key);
-        let is_api_key_correct = apiKeyCheck(key);
         
-        if(is_api_key_correct == true){
-            let auth = new Promise((resolve, reject)=>{
-                let is_parcel_non_empty = clientAuth.non_empty(parcel);
-                let is_username_unique = clientAuth.unique_username(parcel.username);
+        let auth = new Promise((resolve, reject)=>{
+            let is_parcel_non_empty = clientAuth.non_empty(parcel);
+            let is_username_unique = clientAuth.unique_username(parcel.username);
     
-                if(is_parcel_non_empty == true && is_username_unique == true){
-                    resolve('data flow test complete');
-                }
-                else{
-                    let response = accident.get_error();
-                    reject(response);
-                }
-            });
-            auth.then(report => {
-                res.send(report)
-            })
+            if(is_parcel_non_empty == true && is_username_unique == true){
+                resolve('data flow test complete');
+            }
+            else{
+                let response = accident.get_error();
+                reject(response);
+            }
+        });
+        auth.then(report => {
+            res.send(report)
+        })
     
-            .catch(error => {
-                res.send(error);
-            })
-        }
-        else{
-            let payload = accident.get_error();
-            res.send(payload);
-        }
+        .catch(error => {
+            res.send(error);
+        })
 
         accident.clear_log();
     }
