a076aa63a4c6f687b03816225fa36f9e0c6c684b
Ishan Subedi <ikushum@gmail.com>
2019-10-19 13:30:35 +0545

    add dummy documentation

--- routes/signinRoutes.js
+++ routes/signinRoutes.js
@@ -3,9 +3,26 @@ const signinController = require('../controller/signinController');
 
 /**
  * @swagger
+ *
  * /api/signin:
- *    get:
- *      description: This api is used to sign in the users
+ *   get:
+ *     description: Login to the application
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: username
+ *         description: Username to use for login.
+ *         in: json
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password.
+ *         in: json
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: login
  */
 router.get('/', signinController.GET_signin_ROOT);
 
