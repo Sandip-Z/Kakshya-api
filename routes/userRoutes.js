327e9d48c376cd308abcf2f6d299cc72daf73a9a
Ishan Subedi <ikushum@gmail.com>
2019-11-25 16:47:18 +0545

    mbf

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const signupController = require('../controller/userController');
+const userController = require('../controller/userController');
 
-router.get('/',signupController.GET_user_ROOT);
+router.get('/',userController.GET_user_ROOT);
 
 module.exports = router;
