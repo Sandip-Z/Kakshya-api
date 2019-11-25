d158654b87ee7469564c1ad228cccdf510631a99
Ishan Subedi <ikushum@gmail.com>
2019-11-25 14:52:18 +0545

    send 400 error code

--- controller/signinController.js
+++ controller/signinController.js
@@ -8,34 +8,35 @@ class signinController{
             error: 'Only POST request exist for this endpoint',
             documentation : '/api/docs'
         }
+        res.status(400)
         res.send(payload);
     }
 
     async POST_signin_ROOT(req,res){
-
-    let parcel = req.body;
-    let userEmail = parcel.email;
-    let is_user_authentic = await clientAuthModel.is_user_authentic(parcel);
-    if(is_user_authentic === true){
-        let user = clientAuthModel.getUserByEmail(userEmail);
-        jwt.sign({user}, 'tyodinbymcflo', (err, token)=>{
-            if(err){
-                res.json({
-                    status : '403'
-                })
-            }
-            else{
-                res.send({token});
-            }
-        })
+        let parcel = req.body;
+        let userEmail = parcel.email;
+        let is_user_authentic = await clientAuthModel.is_user_authentic(parcel);
+        if(is_user_authentic === true){
+            let user = clientAuthModel.getUserByEmail(userEmail);
+            jwt.sign({user}, 'tyodinbymcflo', (err, token)=>{
+                if(err){
+                    res.json({
+                        status : '403'
+                    })
+                }
+                else{
+                    res.send({token});
+                }
+            })
+        }
+        else{
+            console.log('I am at controllers else block')
+            let cause = accident.get_error();
+            res.status(400)
+            res.send(cause);
+            accident.clear_log();
+        }
     }
-    else{
-        console.log('I am at controllers else block')
-        let cause = accident.get_error();
-        res.send(cause);
-        accident.clear_log();
-    }
-}
 
 }
 
